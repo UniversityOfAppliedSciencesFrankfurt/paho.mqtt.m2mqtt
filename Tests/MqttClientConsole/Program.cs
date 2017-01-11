23674f88ac5f4b3a60eef541e2ab013c6c272484
Rajan0921 <rajan0921@gmail.com>
2017-01-11 23:51:35 +0100

    main code to publish with a topic, and to subcribe with a topic name. Demo is done with existing broker: broker.hivemq.com

--- Tests/MqttClientConsole/Program.cs
+++ Tests/MqttClientConsole/Program.cs
@@ -1,16 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MqttClientConsole
 {
     public class Program
     {
+
+
+        static MqttClient mqttClient = new MqttClient("broker.hivemq.com");
+
         public static void Main(string[] args)
+
         {
-            MqttClient client = new MqttClient("localhost");
+            try
+            {
+                string clientid = "testclientsubscribe";
+
+                mqttClient.Connect(clientid);
+                Console.WriteLine("Connected to MQTT Broker:");
+                Console.WriteLine("Enter the topic name to subscribe:");
+                string topic = Console.ReadLine();
+
+                string[] topics = new string[] { topic };
+
+                byte[] qoslevels = new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE };
+
+                mqttClient.Subscribe(topics, qoslevels);
+                mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+
+                Console.ReadKey();
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error :     " + ex.ToString());
+            }
         }
+
+        private static void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            byte[] response = e.Message;
+
+            Console.WriteLine(Encoding.ASCII.GetString(response));
+        }
+
     }
+
 }
+
--- Tests/MqttServerConsole/Program.cs
+++ Tests/MqttServerConsole/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MqttServerConsole
 {
     public class Program
+
     {
+
+        static MqttClient mqttClient = new MqttClient("broker.hivemq.com");
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello Test");
+
+            string clientid = "testclientpublish";
+
+            mqttClient.Connect(clientid);
+            Console.WriteLine("Connected to MQTT Broker:");
+
+
+            Console.WriteLine("Enter the topics name to publish message:");
+            string topics = Console.ReadLine();
+
+            while (true)
+
+                try
+                {
+
+                    Console.WriteLine("Enter the message to publish:");
+                    string mesage = Console.ReadLine();
+
+                    mqttClient.Publish(topics, Encoding.ASCII.GetBytes(mesage));
+                    Console.WriteLine("publish successful");
+                    // mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+                    Console.ReadKey();
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error :     " + ex.ToString());
+                }
         }
+
+        private static void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            byte[] response = e.Message;
+
+            //Console.WriteLine(Encoding.ASCII.GetBytes(response));
+        }
+
+
     }
 }
