1c70d81b133f840032af9d4183ecaa040d42878b
Raja <Raja@RAJAN>
2017-02-06 17:31:38 +0100

    Publish a topic- MQTT Test- without IoTApi

--- Tests/MqttServerConsole/Program.cs
+++ Tests/MqttServerConsole/Program.cs
@@ -12,10 +12,13 @@ namespace ConsoleServer
     public class Program
     {
         public static void Main(string[] args)
-        {
+
+        {   // already existing mqtt broker "broker.hivemq.com" is used for testing
             MqttClient client = new MqttClient("broker.hivemq.com");
+            // creating universial uniqe code for client identity
             string uniqueserverid = Guid.NewGuid().ToString();
 
+            // method connect() with client id as a parameter- to connect to above assigned broker.
             client.Connect(uniqueserverid);
             try
             {
@@ -32,6 +35,8 @@ namespace ConsoleServer
                     {
                         repeat = false;
                     }
+
+                    // mqtt method publish() to publish a message as bytes
                     client.Publish(topic, Encoding.ASCII.GetBytes(msg));
                 }
                 Console.WriteLine("Publish stopped. Please exit the application");
